4aca80eb6b485f3e7b6f27a3bba7bfc8d49b71d6
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-01-14 09:36:26 +0530

    [Chanrda]-ReverseNumber

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LogicalPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // FibonacciSeries.Fibonac();
+            //PerfectNumeber.Perfect();
+            //PrimeNumber.Prime();
+           //everseNumber.Reverse(245);
+        }
+    }
+}
